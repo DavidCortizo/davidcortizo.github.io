44aa4dcb8b8fdb7d11ddbeb1ec5c661184eba525
DavidCortizo <cortizo7269@gmail.com>
2016-10-06 16:53:53 +0200

    3rd commit material design
    cards and buttons

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
 $(document).ready(function (){
 
   $(".a").click(function () {
-     if ($("#btn-navbar").css('display')!='none')
-     $("#btn-navbar").click();
+    if ($("#btn-navbar").css('display')!='none')
+      $("#btn-navbar").click();
   });
+
+
+  // function cardHover(x) {
+  //   $(x).mouseenter(function(){
+  //       $(this).addClass("mdl-shadow--8dp");
+  //   });
+  //   $(x).mouseleave(function(){
+  //       $(this).removeClass("mdl-shadow--8dp");
+  //   });
+  // }
+ //
+//
+});
+
+function cardHoverIn(element){
+  $(element).addClass("mdl-shadow--8dp card-hover");
+}
+function cardHoverOut(element){
+  $(element).removeClass("mdl-shadow--8dp card-hover");
+}
+
+
+//   $("#section-phi").mouseover(function(){
+//     $("this").addClass(".mdl-shadow--8dp");
+//   }
+// );
 
   // button navigation
   if ( ($(window).height() + 100) < $(document).height() ) {
-     $('#top-button').removeClass('hidden').affix({
-         // how far to scroll down before link "slides" into view
-         offset: {top:100}
+    $('#top-button').removeClass('hidden').affix({
+       // how far to scroll down before link "slides" into view
+       offset: {top:0}
      });
+    $("#top-button").addClass("btn-translate");
   }
   // button navigation
-
-});
